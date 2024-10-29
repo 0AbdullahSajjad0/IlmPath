c08dd501da4ca03cf4b19d1ffc4fdf5289301a7c
SHUJA\DELL <shujaabbas047@gmail.com>
2024-10-29 22:01:45 +0500

    Testing

--- IlmPath/App.js
+++ IlmPath/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to IlmPath</Text>
+      <Text>Welcome to IlmPath-Abdullah Sajjad</Text>
       <StatusBar style="auto" />
     </View>
   );
